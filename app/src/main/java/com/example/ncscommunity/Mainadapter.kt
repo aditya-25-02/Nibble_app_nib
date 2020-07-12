abeb1d25c1eb9e9252bf145a510004a8ff29bcc9
Aditya <adityaverma3471@gmail.com>
2020-07-12 23:25:45 +0530

    ~passed the data from nibblites.kt to Mainadapter.kt to show the data in nibblite section ~intent are to be done UI is also to be done.

--- app/src/main/java/com/example/ncscommunity/Mainadapter.kt
+++ app/src/main/java/com/example/ncscommunity/Mainadapter.kt
@@ -1,42 +1,50 @@
 package com.example.ncscommunity
 
+import android.content.Intent
+import android.content.Intent.ACTION_DIAL
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.nib_row.view.*
 
-class Mainadapter  : RecyclerView.Adapter <CustomViewHolder>() {
+class Mainadapter (val homefeed: Array<Homefeed>)  : RecyclerView.Adapter <CustomViewHolder>() {
 
-    val nib_name = listOf("Aditya verma","Vaibhav Shukla","Ojuswi Rastogi")
-    val club_name = listOf("Programming Club","Designing Club","Programming Club")
-    val image_url = listOf("https://ojuswi.pythonanywhere.com/media/profile_pictures/aditya.jpg","https://ojuswi.pythonanywhere.com/media/profile_pictures/vaibhav.jpg","https://ojuswi.pythonanywhere.com/media/profile_pictures/ojuswi.jpg")
-    val num       = listOf("9528978431","9354545801","8528522733")
     override fun getItemCount(): Int {
-        return 3
+        return homefeed.count()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-
         //how do we create a view
         val layoutInflater = LayoutInflater.from(parent?.context)
         val cellForRow = layoutInflater.inflate(R.layout.nib_row,parent,false)
         return CustomViewHolder(cellForRow)
 
     }
-
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         // parsing data into the view
-        val name =nib_name.get(position)
-        val club =club_name.get(position)
-        val image=image_url.get(position)
-        val number=num.get(position)
 
-        holder?.view?.nib_name?.text = name
-        holder?.view?.nib_club?.text = club
-        Picasso.get().load(image).into(holder.view.nib_image)
+        val obj = homefeed.get(position)
+        holder.view.nib_name.text = obj.full_name
+        holder.view.nib_club.text = obj.club
+        holder.view.nib_work.text = obj.designation
+        holder.view.nib_year.text = obj.year
+        val number = obj.phone_no.trim()
+
+        val image = holder.view.nib_image
+        Picasso.get().load(obj.profile_pic).into(image)
+
+       // holder.view.nib_git.setOnClickListener {
+        //    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+Uri.encode(number)))
+          //  startActivity(intent)
+        //}
     }
 }
+class links ( val link: String ,val website : String)
 class CustomViewHolder (val view : View) : RecyclerView.ViewHolder (view) {
+
 }
